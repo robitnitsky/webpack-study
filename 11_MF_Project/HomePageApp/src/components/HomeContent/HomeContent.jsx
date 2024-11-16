27dd80af0dc1e1ce3b5aaf4b0c66bd31ab775033
Anton Robitnytskyi <robitnitsky.av@gmail.com>
2024-11-16 16:15:56 +0200

    config apps

--- 11_MF_Project/HomePageApp/src/components/HomeContent/HomeContent.jsx
+++ 11_MF_Project/HomePageApp/src/components/HomeContent/HomeContent.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, {Suspense, useEffect, useState} from "react";
 import QuickBooking from "../QuickBooking/QuickBooking.jsx";
+const MovieCard = React.lazy(() => import("components/MovieCard"));
 import "./HomeContent.scss";
 
 const dummyItem = [{name:"Dummy Movie"}]
@@ -7,8 +8,12 @@ const dummyItem = [{name:"Dummy Movie"}]
 const HomeContent = (props) => {
   const [movies, setMovies] = useState(dummyItem);
 
-  useEffect(() => {
+  useEffect(async () => {
     // Add the logic to load the movies from server and set to the state
+    const resp = await fetch("http://localhost:5555/movies");
+    const data = await resp.json();
+    setMovies(data);
+    console.log(data)
   }, []);
 
   const movieClicked = (item) => {
@@ -21,8 +26,7 @@ const HomeContent = (props) => {
     let items = movies.map((item) => {
       return (
         <div onClick={() => movieClicked(item)} key={item.name}>
-          <div>Load the cards Here</div>
-          {/* Load the Movie Card Here */}
+          <MovieCard title={item.name} imageUrl={item.imageUrl}></MovieCard>
         </div>
       );
     });
@@ -34,7 +38,9 @@ const HomeContent = (props) => {
     <div className="home-content-container">
       <QuickBooking></QuickBooking>
       <div className="movies-container">
-        {renderMovieList()}
+        <Suspense fallback={null}>
+          {renderMovieList()}
+        </Suspense>
       </div>
     </div>
   );
